eb609f3a6d30744b89042647a4246f6d143d81db
Paulo <git@pauloo27.dev>
2026-01-10 01:45:49 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
-use mlua::Lua;
+use std::collections::HashMap;
+
+use mlua::{Function, Lua};
 
 fn main() {
     let lua = Lua::new();
+    let globals = lua.globals();
+
+    let ffff = lua
+        .create_function(|_, dict: HashMap<String, Function>| {
+            println!("i have been called with {dict:?}");
+            let cb = dict.get("cb").unwrap();
+            cb.call::<()>(()).unwrap();
+            Ok(())
+        })
+        .expect("Fn");
+
+    globals.set("window", ffff).expect("ffff");
+
     let c = lua.load(
         r#"
-        return _VERSION
+        window {
+            title = "bar";
+            cb = function()
+                print(_VERSION)
+            end
+        }
         "#,
     );
-    let res = c.eval::<String>().expect("lower your expectations");
-    println!("{}", res);
+
+    c.exec().unwrap();
+
+    /*
+    let res = c
+        .eval::<HashMap<String, String>>()
+        .expect("lower your expectations");
+    println!("{:?}", res);
+    */
 }
