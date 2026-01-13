d7d5db83de423781c06831b94892d3e023b5ac10
Paulo <git@pauloo27.dev>
2026-01-12 22:39:51 -0300

    feat: set config path for imports/css files

--- src/lua/mod.rs
+++ src/lua/mod.rs
@@ -13,10 +13,31 @@ pub struct LuaEnv {
     sender: Sender<UICommands>,
 }
 
+fn get_config_dir() -> PathBuf {
+    if let Ok(xdg_config) = std::env::var("XDG_CONFIG_HOME") {
+        PathBuf::from(xdg_config).join("smsh")
+    } else if let Ok(home) = std::env::var("HOME") {
+        PathBuf::from(home).join(".config").join("smsh")
+    } else {
+        PathBuf::from(".config/smsh")
+    }
+}
+
 impl LuaEnv {
     pub fn new(ui: &UI) -> AnyResult<Self> {
         let lua = Lua::new();
         let globals = lua.globals();
+
+        // Set up Lua package.path to include config directory
+        let config_dir = get_config_dir();
+        let config_dir_str = config_dir.to_string_lossy();
+        let package_path = format!("{}/?.lua;{}/?/init.lua;", config_dir_str, config_dir_str);
+        lua.load(&format!(
+            "package.path = '{}' .. package.path",
+            package_path
+        ))
+        .exec()
+        .map_err(|e| anyhow::anyhow!("Failed to set package.path: {}", e))?;
 
         let sender = ui.get_sender();
         let sender_clone = sender.clone();
@@ -30,12 +51,21 @@ impl LuaEnv {
             })
             .map_err(|e| anyhow::anyhow!("Failed to create window function: {}", e))?;
 
+        let config_dir_clone = config_dir.clone();
         let load_css_fn = lua
             .create_function(move |_, path: String| {
                 let expanded_path = shellexpand::full(&path)
                     .map_err(|e| mlua::Error::runtime(format!("Failed to expand path: {}", e)))?;
+
+                let path_buf = PathBuf::from(expanded_path.as_ref());
+                let final_path = if path_buf.is_absolute() {
+                    path_buf
+                } else {
+                    config_dir_clone.join(path_buf)
+                };
+
                 sender_clone
-                    .send(UICommands::LoadCSS(PathBuf::from(expanded_path.as_ref())))
+                    .send(UICommands::LoadCSS(final_path))
                     .map_err(|_| {
                         mlua::Error::runtime("Failed to send load_css command to UI thread")
                     })?;
